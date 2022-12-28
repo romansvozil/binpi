bb05a734e88f42c2677f9d23a559d07073da02f1
Roman <romdrak@email.cz>
2022-12-29 00:06:25 +0100

    chore: bump version in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
       name='binpi',
-      version='0.1.7',
+      version='0.1.8',
       description='Simple library for de/serializing binary data',
       readme = "README.md",
       long_description=open("README.md", "r").read(),
